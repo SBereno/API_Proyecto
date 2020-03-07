1fec5d3baebf1e014329f8f3f9944b99d02b1076
SBereno <percival1999@gmail.com>
2020-03-08 00:14:20 +0100

    FIX Login ahora es post

--- routes/users.route.js
+++ routes/users.route.js
@@ -6,7 +6,7 @@ const router = express.Router({ mergeParams: true });
 
 const usersController = require('../controllers/users.controller');
 
-router.route('/login').get(usersController.loginAttempt);
+router.route('/login').post(usersController.loginAttempt);
 
 router.route('/signup').post(usersController.signUp);
 
